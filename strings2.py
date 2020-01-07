b53c41a391d324b5833c246757369efdd8ae070e
Camilo Andrés Concha Agudelo <cconcha@CamiloConcha.local>
2020-01-07 11:47:41 -0500

    Starting with conditionals

--- strings2.py
+++ strings2.py
@@ -1,7 +1,17 @@
 word1 = 'Good'
 word2 = 'Evening'
 end1 = word1[2] + word1[3]
+half2 = len(word2)/2
+half2index = len(word2)//2
 
 word = 'Python'
 
-print(word[ 2:5 ])
+
+
+print('First way \n ' + word[0:2])
+print('Second way \n ' + word[:2])
+print('Third way \n ' + word[2:4])
+print('Fourth way \n ' + word[2:])
+
+print('Halfway index '+ str(half2index))
+print('Halfway index '+ str(half2index))
