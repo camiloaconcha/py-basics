c105c76f1d528557b539ad98a476e60227188604
Camilo Andrés Concha Agudelo <cconcha@CamiloConcha.local>
2020-01-07 14:08:46 -0500

    Added conditionals and Input methods

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,14 @@
-value = 2
-day = 'Monday'
+value = int(input('Enter the number to check'))
+day = input('Enter a day of the week ! = ')
 
-if value > 0 and value%2 == 0:
+if value > 0 and value % 2 == 0:
     print("This is positive")
 elif value == 0:
     print('Zero')
 else:
     print('This is negative')
+
+if day == 'Sunday' or day == 'Saturday':
+    print('Weekend')
+else:
+    print('Week')
