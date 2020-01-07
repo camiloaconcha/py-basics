e1543a51f082d4f170f986c18e1005f0f0d996f2
Camilo Andrés Concha Agudelo <cconcha@CamiloConcha.local>
2020-01-07 11:28:47 -0500

    Working with strig

---
+++ strings.py
@@ -0,0 +1,21 @@
+first_name = 'Monty'
+
+last_name = 'Python'
+
+full_name = first_name + ' ' + last_name
+
+# PRINTING
+
+#print(first_name, last_name)
+# print(full_name)
+
+# Concatenation
+
+description = 'sketch comedy group'
+year = 1969
+famous_sketch = "Hell's Grannies"
+
+sentence = full_name + ' is a ' + description + ' formed in ' + \
+    str(year) + ' known for sketches such as: ' + famous_sketch
+    
+print(len(sentence))
---
+++ strings2.py
@@ -0,0 +1,7 @@
+word1 = 'Good'
+word2 = 'Evening'
+end1 = word1[2] + word1[3]
+
+word = 'Python'
+
+print(word[ 2:5 ])
